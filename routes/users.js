11de353326e2bfb31e7f8f490e1e42a501bc0b29
Carine Croteau <carine.croteau@gmail.com>
2022-11-24 19:48:58 -0500

    ajout bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
 
 // ajout du module de génération de tokens jwt
 const jwt = require('jsonwebtoken');
@@ -19,7 +20,7 @@ function authentifierJeton(req, res, next){
   console.log("authentification en cours...");
   console.log(req.cookies);
   if(req.cookies.jeton) {
-      console.log("Jeton" + req.cookies.jeton);
+      console.log("Jeton : " + req.cookies.jeton);
   }
 
   if(!req.cookies.jeton) return res.status('401').json({erreur:'Jeton d\'authentification manquant'});
@@ -42,19 +43,39 @@ router.get('/', authentifierJeton, function(req, res, next) {
 router.post('/login', function(req, res, next) {
   // Récupérer nomUtilisateur motDePasse  
   const nomUtilisateur = req.body.nomUtilisateur;
-  const motDePasse = req.body.motDePasse;
+  const motDePasse = req.body.motDePasse;   // utiliser bcrypt et récupérer le mot de passe crypté dans la base de données
 
   // Dans cet exemple, supposons que le mot de passe valide est qwerty
   if(motDePasse == 'querty') {
     const jeton = jwt.sign(nomUtilisateur, process.env.SECRET);
     console.log("Création du jeton: " + jeton);
-    res.cookie('jeton', jeton);
+    let expiration = new Date() + (10 * 60 * 1000);
+    console.log('expiration du cookie:' + expiration);
+
+    res.cookie('jeton', jeton, {
+        secure: process.env.NODE_ENV !== "development",
+        httpOnly: true,
+        expires: expiration
+    });    
+
     res.json({login:'success'});
   } else {
     res.clearCookie('jeton');
     res.status('401').json('Erreur d\'authentification');
   }
 
+});
+
+router.get('/bcrypt', function (req, res, next) {
+  let myPlaintextPassword = 'myPlaintextPassword';
+  let saltRounds = 10;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    console.log('salt : ' + salt)
+    bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+      console.log('hash: ' + hash); 
+      res.send(hash);
+    });
+  });
 });
 
 
